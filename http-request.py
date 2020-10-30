45904cdae7f6ad5a3a9348b6901de302ae1a02df
Touhid Mia <hmtm.cse@gmail.com>
2020-10-30 19:59:33 +0600

    request

---
+++ http-request.py
@@ -0,0 +1,24 @@
+import requests
+
+
+response = requests.get('http://localhost:8088/')
+print("Code: " + str(response.status_code) + " Body: " + response.text)
+
+response = requests.get('http://localhost:8088/request-methods')
+print("Code: " + str(response.status_code) + " Body: " + response.text)
+
+response = requests.post('http://localhost:8088/request-methods')
+print("Code: " + str(response.status_code) + " Body: " + response.text)
+
+response = requests.patch('http://localhost:8088/request-methods')
+print("Code: " + str(response.status_code) + " Body: " + response.text)
+
+response = requests.delete('http://localhost:8088/request-methods')
+print("Code: " + str(response.status_code) + " Body: " + response.text)
+
+
+response = requests.post('http://localhost:8088/json-data', json= {'username': 'Touhid', 'password': '123456'})
+print("Code: " + str(response.status_code) + " Body: " + response.text)
+
+response = requests.post('http://localhost:8088/json-data-to-json', json= {'username': 'Touhid', 'password': '123456'})
+print("Code: " + str(response.status_code) + " Body: " + response.text)
